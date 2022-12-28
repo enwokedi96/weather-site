75e246e0be579baace93d242b275c07539401373
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-28 02:57:05 +0000

    edited css paths

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var searchLimit = 1;
-    var apiKey = "ebc857fdadc713393f458859461c4d71";
+    var apiKey = ""; //"";
     var lonLatURL = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=${searchLimit}&appid=${apiKey}`
     var queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     var countryInput = $('').text;
