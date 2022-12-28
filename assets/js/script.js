024215796c7bbc133d20730b36ca1c9fed408f1d
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-28 03:40:29 +0000

    loaded relevant tags, rearraned var calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,29 @@
 $(document).ready(function() {
+    var lon; var lat;
     var lonLatURL;
     var countryInput;
     var searchLimit = 1;
+    var today = $('#today')
+    var forecast = $('#forecast')
     var apiKey = ""; //"";
 
     $('#search-button').on('click',function(event){
         event.preventDefault();
         countryInput = $('#search-input').val();
-        console.log(countryInput)
-        lonLatURL = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=ebc857fdadc713393f458859461c4d71' //`http://api.openweathermap.org/geo/1.0/direct?q=${countryInput}&limit=${searchLimit}&appid=${apiKey}`;
+        console.log(countryInput);
+        lonLatURL = `http://api.openweathermap.org/geo/1.0/direct?q=${countryInput}&limit=${searchLimit}&appid=${apiKey}` 
+        // load 
         $.ajax({
           url: lonLatURL,
           method: "GET"
         }).then(function(response) {
-          console.log(JSON.stringify(response));
+          lon = response[0].lon;
+          lat = response[0].lat;
+          console.log(response,lon,lat)//(JSON.stringify(response));
         })
     })
 
+    
     //var queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
 }
