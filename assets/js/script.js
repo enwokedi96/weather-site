80e62e66d151f7e5b8c421bd5a62f68f66f9a700
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-28 04:29:37 +0000

    successfully nested API calls , weather data loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,43 @@
 $(document).ready(function() {
-    var lon; var lat;
+    var lon; 
+    var lat;
     var lonLatURL;
+    var queryURL
     var countryInput;
     var searchLimit = 1;
     var today = $('#today')
     var forecast = $('#forecast')
-    var apiKey = ""; //"";
-
+    var apiKey = ""; 
+    
+    // get location on click event 
     $('#search-button').on('click',function(event){
         event.preventDefault();
         countryInput = $('#search-input').val();
         console.log(countryInput);
         lonLatURL = `http://api.openweathermap.org/geo/1.0/direct?q=${countryInput}&limit=${searchLimit}&appid=${apiKey}` 
-        // load 
+        
+        // get locations latitude and longitude 
         $.ajax({
           url: lonLatURL,
           method: "GET"
         }).then(function(response) {
           lon = response[0].lon;
           lat = response[0].lat;
-          console.log(response,lon,lat)//(JSON.stringify(response));
-        })
-    })
+          //console.log(lon,lat) //(JSON.stringify(response));
+          queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
-    
-    //var queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+          // load weather and forecasts using longitude and latitude
+          $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).done(function(result) {
+            console.log(result); //(JSON.stringify(response));
+            today.append(`<h2>Today (${moment().format("MMM Do YY")})</h2>`);
+
+          })
+        })
+        
+    })
 
 }
 )
