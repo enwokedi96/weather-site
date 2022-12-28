d1c270545f17601a754e86354b19fe9287b14678
ezechukwu nwokedi <israellambert96@gmail.com>
2022-12-28 03:20:23 +0000

    search function now successfully loads lat-lon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
+    var lonLatURL;
+    var countryInput;
     var searchLimit = 1;
     var apiKey = ""; //"";
-    var lonLatURL = `http://api.openweathermap.org/geo/1.0/direct?q=London&limit=${searchLimit}&appid=${apiKey}`
-    var queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    var countryInput = $('').text;
 
-    $.ajax({
-        url: lonLatURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-      })
+    $('#search-button').on('click',function(event){
+        event.preventDefault();
+        countryInput = $('#search-input').val();
+        console.log(countryInput)
+        lonLatURL = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=ebc857fdadc713393f458859461c4d71' //`http://api.openweathermap.org/geo/1.0/direct?q=${countryInput}&limit=${searchLimit}&appid=${apiKey}`;
+        $.ajax({
+          url: lonLatURL,
+          method: "GET"
+        }).then(function(response) {
+          console.log(JSON.stringify(response));
+        })
+    })
+
+    //var queryURL = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
 }
 )
